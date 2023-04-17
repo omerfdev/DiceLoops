6f459d2bf286ec471bfb74b8d52e2317d4aa28ec
kurumsal <kurumsal@KDK-103-PC09-YZ>
2023-04-17 16:48:45 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int i = 0;
+bool alwaysRun=true;
+
+while (alwaysRun)
+{
+    Random rnd = new Random();
+    int xface = rnd.Next(7);
+    int yface = rnd.Next(7);
+    i++;
+    while (xface == 6 && yface == 6 && alwaysRun==true)
+    {
+        Console.WriteLine(i);
+        alwaysRun = false;
+
+    }
+
+}
+Console.ReadLine();
